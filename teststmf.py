180eec3d1c652656fac3a775713491fe01cde0c4
Cyril Plisko <cyril.plisko@grigale.com>
2009-11-30 21:37:26 +0200

    Print state

--- teststmf.py
+++ teststmf.py
@@ -1,4 +1,13 @@
 #!/bin/python2.6
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+#
 # Copyright 2009 Grigale, Ltd. All rights reserved.
 # Use is subject to license terms.
 #
@@ -11,4 +20,5 @@ print "Excersizing stmfGetState()"
 
 operationalState, configState = stmf.getstate()
 
-print operationalState, configState
+print " STMF service : %s" % stmf.STMF_STATES[operationalState]
+print " STMF config  : %s" % stmf.STMF_STATES[configState]
